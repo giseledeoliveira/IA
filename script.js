26957997414823de8aacc50968fb05e521aacb73
gisele oliveira <129314526+giseledeoliveira@users.noreply.github.com>
2024-08-30 11:13:40 -0300

    script.js

--- script.js
+++ script.js
@@ -6,66 +6,66 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Ao realizar algumas pesquisas, você se depara com uma nova tecnologia, a Inteligencia Artificial. Ela atua na reprodução de padrões e comportamentos humanos por meio de dispositivos e programas computacionais Qual seu primeiro pensamento?",
+        enunciado: "Ao realizar algumas pesquisas, você se depara com uma nova tecnologia, a Inteligencia Artificial. Ela atua na reprodução de padrões e comportamentos humanos por meio de dispositivos e programas computacionais. Qual seu primeiro pensamento?",
         alternativas: [
             {
                 texto: "Que terrivel!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: "No início ficou apreencivo com a novidade."
             },
             {
                 texto: "Que extraordinario!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: "Ficou curioso e queria aprender mais."
             }
         ]
     },
     {
-        enunciado: "Com essa inovação tão presente no dia a dia, um professor de Programação pede que você crie um projeto sobre a famosa IA . Qual atitude você tomaria ?",
+        enunciado: "Com essa inovação tão presente no dia a dia, um professor de Programação pede que você crie um projeto sobre a famosa IA . Qual atitude você tomaria?",
         alternativas: [
             {
                 texto: "Realizo pesquisas para o aprofundamento do assunto, com base em pesquisas na internet.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                afirmacao: "Conseguiu utilizar de forma fácil a IA."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas de turma, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Sentiu mais facilidade em utilizar seus próprios conhecimentos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Após a elaboração desse projeto, o professor promove um debate para que os alunos apontem pontos positivos e negativos sobre o assunto em relação aos trabalhadores, como você se posiciona?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e aprimorar habilidades humanas.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e a melhorando o mercado de trabalho."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Me posiciono contra. Pois, penso em quantos trabalhadores perderão seus empregos pela falta de mão de obra.",
                 afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Você sabe quais são os tipos de IA?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Ainda não conheço muito o assunto,quais são?",
+                afirmacao: " Máquinas reativas, memória limitada, teoria da mente e autoconsciente."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                texto: "Simm, meu projeto me proporcionou uma pesquisa aprimorada sobre o assunto. Mas atualmente qual o pais que mais investem nessas tecnologias?",
                 afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "  A inteligencia Artificial possui 4 tipos, máquinas reativas, memória limitada, teoria da mente e autoconsciente.Pelo décimo segundo ano consecutivo, a Suíça é o país mais inovador do mundo, de acordo com o relatório IGI Link externo, abra em uma nova aba.  ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                texto: "Que demais!",
                 afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                texto: "Não havia pesquisado sobre isso",
                 afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
         ]
